872e226653e7c40d9e07a842b38e0634377e3e76
Roshan Kumar <roshankumarrtk81@gmail.com>
2024-04-22 16:25:22 +0530

    dijkastra added

---
+++ dijkastra.java
@@ -0,0 +1,19 @@
+public long[] dijkastra(int src, ArrayList<Pair>[] graph){
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+    boolean[] visited = new boolean[graph.length];
+    long[] dis = new long[graph.length];
+    Arrays.fill(dis,(long)1e15);
+    pq.add(new Pair(src,0));
+
+    while(pq.size()>0){
+      Pair rem = pq.remove();
+      if(!visited[rem.src]){
+          visited[rem.src]=true;
+          dis[rem.src]=rem.wt;
+          for(Pair child:graph[rem.src]){
+            pq.add(new Pair(child.src, rem.wt+child.wt));
+          }
+      }
+    }
+    return dis;
+  }
